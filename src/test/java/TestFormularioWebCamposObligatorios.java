6ddf84786f5b92dfe24b93c9ff2b2ef2cb32ebc6
daylaserves <74622380+daylaserves@users.noreply.github.com>
2021-11-16 16:41:42 -0500

    Add files via upload

--- src/test/java/TestFormularioWebCamposObligatorios.java
+++ src/test/java/TestFormularioWebCamposObligatorios.java
@@ -20,50 +20,42 @@ public class TestFormularioWebCamposObligatorios {
 
     @Test
     public void testUntitledTestCase() throws Exception {
-        driver.get("https://docs.google.com/forms/u/2/d/e/1FAIpQLScXkFWJEaQg62bP0-modAIrED3yG5jz7bo4pzHrwIfUW-K9Zw/viewform");
-        //driver.findElement(By.xpath("//div")).click();
-        //driver.findElement(By.xpath("//input[@type='text']")).clear();
-        //driver.findElement(By.xpath("//input[@type='text']")).sendKeys("");
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).clear();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).sendKeys("");
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[6]/div/div")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span")).click();
+        driver.get("https://forms.gle/1MbYv8WmGnhu6K7D9");
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span/span")).click();
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
         driver.findElement(By.xpath("//input[@type='text']")).clear();
-        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("102554458");
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("1036645344");
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).click();
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div")).click();
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div")).click();
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div")).click();
+        driver.findElement(By.xpath("//input[@type='email']")).click();
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).clear();
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Daniela López");
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).clear();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).sendKeys("Adolfo Cardenas");
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).sendKeys("123654-7");
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).click();
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).clear();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).sendKeys("147852369");
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).clear();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).sendKeys("Cohan");
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).sendKeys("COHAN");
         driver.findElement(By.xpath("//input[@type='email']")).click();
         driver.findElement(By.xpath("//input[@type='email']")).clear();
-        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("adolfo@cohan.com");
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div/div/div/input")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div/div/div/input")).clear();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div/div/div/input")).sendKeys("45");
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span/span")).click();
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("dlopez29@misena.edu.co");
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span")).click();
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
         driver.findElement(By.xpath("//div[@id='i8']/div[3]/div")).click();
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
         driver.findElement(By.xpath("//div[@id='i24']/div[3]/div")).click();
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
-        driver.findElement(By.xpath("//div[@id='i40']/div[3]/div")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span")).click();
-        driver.findElement(By.xpath("//div[@id='i56']/div[3]/div")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div")).click();
-        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span")).click();
-        driver.findElement(By.xpath("//div[@id='i72']/div[3]/div")).click();
+        driver.findElement(By.xpath("//div[@id='i43']/div[3]/div")).click();
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
-        driver.findElement(By.xpath("//div[@id='i85']/div[3]/div")).click();
+        driver.findElement(By.xpath("//div[@id='i59']/div[3]/div")).click();
         driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
+        driver.findElement(By.xpath("//div[@id='i75']/div[3]/div")).click();
+        driver.findElement(By.xpath("//div[@id='i88']/div[3]/div")).click();
+        driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span")).click();
     }
 
     @After
--- src/test/java/TestFormularioWebExitoso.java
+++ src/test/java/TestFormularioWebExitoso.java
@@ -20,28 +20,31 @@ public class TestFormularioWebExitoso {
 
         @Test
         public void testUntitledTestCase() throws Exception {
-            driver.get("https://docs.google.com/forms/u/2/d/e/1FAIpQLScXkFWJEaQg62bP0-modAIrED3yG5jz7bo4pzHrwIfUW-K9Zw/viewform");
+            driver.get("https://forms.gle/1MbYv8WmGnhu6K7D9");
             driver.findElement(By.xpath("//div[@id='i5']/div[3]/div")).click();
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span/span")).click();
             driver.findElement(By.xpath("//input[@type='text']")).click();
             driver.findElement(By.xpath("//input[@type='text']")).clear();
-            driver.findElement(By.xpath("//input[@type='text']")).sendKeys("12345");
+            driver.findElement(By.xpath("//input[@type='text']")).sendKeys("1036645344");
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
+            driver.findElement(By.xpath("//input[@type='text']")).click();
+            driver.findElement(By.xpath("//input[@type='text']")).clear();
+            driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Daniela López Sossa");
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div")).click();
             driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).click();
             driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).clear();
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).sendKeys("Daniela López");
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input")).sendKeys("147852-9");
             driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).click();
             driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).clear();
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).sendKeys("987456123");
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).click();
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).clear();
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div/div/input")).sendKeys("Cohan");
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/div/div/input")).sendKeys("COHAN");
             driver.findElement(By.xpath("//input[@type='email']")).click();
             driver.findElement(By.xpath("//input[@type='email']")).clear();
             driver.findElement(By.xpath("//input[@type='email']")).sendKeys("dlopez29@misena.edu.co");
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div/div/div/input")).click();
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div/div/div/input")).clear();
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div/div/div/input")).sendKeys("28");
-            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div/span/span")).click();
-            driver.findElement(By.xpath("//div[@id='i5']/div[3]/div")).click();
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div/div/div/input")).click();
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div/div/div/input")).clear();
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div/div/div/input")).sendKeys("28");
+            driver.findElement(By.xpath("//form[@id='mG61Hd']/div[2]/div/div[3]/div/div/div[2]/span/span")).click();
+            driver.findElement(By.xpath("//div[@id='i8']/div[3]/div")).click();
             driver.findElement(By.xpath("//div[@id='i24']/div[3]/div")).click();
             driver.findElement(By.xpath("//div[@id='i40']/div[3]/div")).click();
             driver.findElement(By.xpath("//div[@id='i56']/div[3]/div")).click();
